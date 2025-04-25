89e07e6f9e8743b4c7c38824f3ef43192eea950c
WireNext <wirennextt@gmail.com>
2025-04-25 11:46:59 +0200

    Update procesar_geojson.py

--- procesar_geojson.py
+++ procesar_geojson.py
@@ -102,15 +102,17 @@ def extract_and_process_tar(tar_path='avisos.tar'):
 
         # Guardar todos los features en un solo GeoJSON
         if all_features:
-            geojson_data = {
+           geojson_data = {
                 "type": "FeatureCollection",
-                "features": all_features
+                "features": all_features  # puede estar vacío
             }
             with open(SALIDA_GEOJSON, 'w') as geojson_file:
                 json.dump(geojson_data, geojson_file, indent=4)
-            print("✅ GeoJSON generado correctamente con todos los avisos.")
-        else:
-            print("⚠️ No se encontraron avisos válidos en los archivos XML.")
+
+            if all_features:
+                print("✅ GeoJSON generado correctamente con todos los avisos.")
+            else:
+             print("⚠️ No se encontraron avisos válidos. GeoJSON vacío generado.")
 
     except Exception as e:
         print(f"Error al extraer y procesar el archivo TAR: {e}")
